cb34e590427fa39d023e836dcf17af18a91de31b
LucasGambim <lucas.gambim.guedes@gmail.com>
2023-08-06 02:02:56 -0300

    adicionado resposividade na pagina do palestrantes

--- site_camc/src/components/palestrantes/index.jsx
+++ site_camc/src/components/palestrantes/index.jsx
@@ -1,45 +1,35 @@
-import fotoavatar from '../../assets/fotoavatar.jpg'
+import fotoavatar from '../../assets/fotominha1.png'
 
-export function Palest() {
+export function Palest(props) {
+
     return (
-        <>
-                <div className="grid grid-cols-3 bg-slate-800 flex h-96 w-30">
-                    <div className="col-start-1 bg-slate-800">
- 
-                            <img
-                                src={fotoavatar}
-                                className="
-                                h-96 w-96 rounded-l-lg rounded-tr-lg">
-                            </img>
+        <> 
+        < div className = "group max-w-full h-64 sm:my-40 my-6 flex justify-center items-center" > 
+            <div className=  "sm:max-w-5xl sm:h-5/6 w-4/5 h-3/4 bg-sky-950 rounded-lg  duration-300 flex md:flex-row  flex-wrap">
+                <div className=' bg-sky-950 w-1/4 h-full rounded-l-lg'>
+                <img
+                        src={props.foto}
+                        className="
+                                sm:h-40 sm:w-40 md:h-48 md:w-48 lg:h-52 lg:w-52 xl:h-60 xl:w-60 
+                                xl:group-hover:h-[16rem] xl:group-hover:w-96 lg:group-hover:h-56 lg:group-hover:w-56 
+                                rounded-lg lg:-translate-x-1/4 lg:-translate-y-1/4 md:-translate-y-1/4 -translate-x-1/4 translate-y-1/4">
+                    </img>
+                </div> 
+                <div className='rounded-r-lg h-full w-3/4 flex flex-col'>
+                    <div className=' bg-cyan-700 rounded-tr-lg h-1/6 w-full items-center flex'>
+                        <p className=' text-lg al px-3 font-mono font-bold ' >Nome do Palestrante</p>
                     </div>
-                    <div className="col-start-2 col-span-3 grid">
-                        <div className='grid grid-rows-6'>
-                            <div className='row-start-1 bg-gradient-to-r from-white-900 to-slate-800'>
-                                <p className="text-lx text-white-950 font-bold pl-4 pt-4">
-                                    NOME
-                                </p>
-                            </div>
-                            <div className='row-start-2 row-span-6 row-end-6 rounded-tr-lg bg-white'>
-                                <p className="text-base text-neutral-950 text-justify pr-8 pl-4 pt-4">No seria cosa neguna en lo món, senyor, per fort que fos, que yo no manifestàs a la majestat vostra per 
-                                    la molta amor e voluntat que tinch de servir-vos. Per bé que sia cosa de gran dolor, 
-                                    yo vull obeir lo manament que·m fa la altesa vostra. Car yo viu a la sereníssima senyora emperadriu e 
-                                    la excelsa princessa, les II là en taula posades, e sentí un 
-                                    fort e profunde suspir que la senyora emperadriu lançà; pensí que suspirava per aquell que havia parit. 
-                                    En aquell cars, la mia ànima, de pietat sentí dolor inextimable. 
-                                    
-                                    </p>
-                            </div>
-                            <div className='row-start-6 bg-sky-800 rounded-br-lg'>
-                                   <p className='text-lg text-white-950 pl-4 pt-4'>Lattes:
-                                   </p>
-                                </div>
-                        
-                        </div>
+                    <div className='h-3/4 w-full bg-white'>
+                        <p className='text-start text-xs sm:text-xs px-3  
+                            md:text-sm lg:text-base  text-zinc-900 font-bold tracking-tight'>{props.descricao} {/*445 caracters */}
+                        </p>
                     </div>
-                    
-                </div>
-        </>
-    )
+                    <div className=' bg-sky-950 w-full h-1/4 rounded-br-lg flex flex-row items-center' >
+                        <p className='text-start text-xs sm:text-base  md:text-lg px-3 font-mono font-semibold tracking-tight'>{props.lattes}</p>
 
-
-}
+                    </div>
+                </div>     
+            </div>
+    </div>
+</>
+    )}
--- site_camc/src/pages/Palestrantes/index.jsx
+++ site_camc/src/pages/Palestrantes/index.jsx
@@ -1,30 +1,35 @@
 import { Layout } from "../../components/Layout";
 import { Palest } from "../../components/palestrantes";
 import { Link } from "../../components/Link";
-
+import fotoavatar from '../../assets/fotominha1.png'
+import { useRef } from 'react'
 
 export function Palestrantes() {
+    
 
     return(
         <>
             <Layout>
-                <div className="grid grid-cols-6 py-20">
-                    <div className="col-start-2 col-span-4 rounded-md">
-                        <Palest />
-                    </div>
+                <div className=" h-screen w-screen">
+                        <Palest descricao='Bacharel em Física pela Universidade Federal do Rio Grande do Sul (UFRGS). 
+                            Supervisor de Proteção Radiológica certificado pela CNEN em instalação na área de medicina nuclear. 
+                            Especialista em Física Médica - Medicina Nuclear pelo Hospital de Clínicas de Porto Alegre (HCPA). 
+                            Mestrando em Radioproteção e Dosimetria no Instituto de Radioproteção e Dosimetria (IRD/CNEN). 
+                            Sócio Administrador da empresa Diferencial Física Médica. 
+                            ' lattes='Lattes: http://lattes.cnpq.br/5638604094028913' foto={fotoavatar}/>
+                        <Palest descricao='Lucas Gambim é estudantes de Física Médica e possui.
+                            experiencia com programação em linguagens como c++ python e React, 
+                            foi estudante de informática e participa de uma pesquisa,
+                            envoveldo simulações monte carlo e c++
+                            porro maxime totam' lattes='Lattes: http://lattes.cnpq.br/5638604094028913' foto={fotoavatar}/>
+                        <Palest descricao='Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                             Voluptas alias eos quaerat in non recusandae numquam maiores, 
+                             porro maxime totam provident mollitia tempora accusamus earum assumenda placeat autem et deserunt.
+                             accusamus earum assumenda placeat autem et deserunt
+                             porro maxime totam' lattes='Lattes: http://lattes.cnpq.br/5638604094028913' foto={fotoavatar}/>
                 </div>
-                <div className="grid grid-cols-6 py-20">
-                    <div className="col-start-2 col-span-4">
-                        <Palest />
-                    </div>
-                </div>
-                <div className="grid grid-cols-6 py-20">
-                    <div className="col-start-2 col-span-4">
-                        <Palest />
-                    </div>
-                </div>
-                
             </Layout>
+            
         </>   
         
     )
