79ebed685dbbd9434a7ab38d7130df40a6c218bc
LucasGambim <lucas.gambim.guedes@gmail.com>
2023-08-24 20:08:15 -0300

    manutenção da respossividade da pagina dos palestrantes

--- site_camc/src/components/Header/index.jsx
+++ site_camc/src/components/Header/index.jsx
@@ -14,17 +14,19 @@ export function Header() {
     }
 
     return <header
-        className="sticky top-0 z-30 flex dark:bg-slate-900 bg-zinc-950 py-4 md:px-10 px-7 lg:h-32 text-center border-b border-solid
+        className="sticky top-0 z-30 flex dark:bg-slate-900 bg-zinc-950 py-4 lg:px-10 px-7 lg:h-32 text-center border-b border-solid
         border-gray-600 font-bold  lg:w-full w-full md:w[800px]">
         <div
             className="container mx-auto flex h-30 
                 justify-between
                 md:justify-center
+                
                 items-center">
             <a href="/home">
                 <img
                     src={logoFisMed}
                     className="justify-start scale-150 
+                    lg:h-28 lg:w-30 
                     h-28 w-30 
                     pt-6 pr-6"
                 >
